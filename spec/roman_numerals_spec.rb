d46a93438b5a454dda1933600522972491f46407
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-06 08:18:56 +0200

    changes the examples description

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -9,51 +9,56 @@ def convert(int)
   when 21 then "XXI"
   when 50 then "L"
   when 100 then "C"
-
+  when 500 then "D"
+  when 1000 then "M"
   end
 
 end
 
 
 RSpec.describe "Roman numerals" do
-  it "returns I when an integer 1 is an argument" do
+  it "converts 1 for 'I'" do
     expect(convert(1)).to eq("I")
   end
 
-  it "returns II when an integer 2 is an argument" do
+  it "converts 2 for 'II'" do
     expect(convert(2)).to eq("II")
   end
 
-  it "returns III when an integer 3 is an argument" do
+  it "converts 3 for 'III'" do
     expect(convert(3)).to eq("III")
   end
 
-  it "returns IV when an integer 4 is an argument" do
+  it "converts 4 for 'IV'" do
     expect(convert(4)).to eq("IV")
   end
 
-  it "returns V when an integer 5 is an argument" do
+  it "converts 5 for 'V'" do
     expect(convert(5)).to eq("V")
   end
 
-  it "returns IX when an integer 9 is an argument" do
+  it "converts 9 for 'IX'" do
     expect(convert(9)).to eq("IX")
   end
 
-  it "returns XXI when an integer 21 is an argument" do
+  it "converts 21 for 'XXI'" do
     expect(convert(21)).to eq("XXI")
   end
 
-  it "returns L when an integer 50 is an argument" do
+  it "converts 50 for 'L'" do
     expect(convert(50)).to eq("L")
   end
 
-  it "returns C when an integer 100 is an argument" do
+  it "converts 100 for 'C'" do
     expect(convert(100)).to eq("C")
+  end
 
-  it "returns D when an integer 500 is an argument" do
+  it "converts 500 for 'D'" do
     expect(convert(500)).to eq("D")
   end
 
+  it "converts 1000 for 'M'" do
+    expect(convert(1000)).to eq("M")
   end
+
 end
