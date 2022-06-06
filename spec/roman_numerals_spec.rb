410db1c15c0e4c6c2b40682e3edb749948ede5d0
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-06 10:58:14 +0200

    used 'context' keyword  to organize the examples

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,5 +1,5 @@
 class Roman
-  symbols = [
+  Symbols = [
     ["I", 1],
     ["II", 2],
     ["III", 3],
@@ -14,7 +14,7 @@ class Roman
   ]
   def convert(int)
     result = " "
-    symbols.each do |roman, integer|
+    Symbols.each do |roman, integer|
       while int >= integer
         result += roman
         int -= integer
@@ -30,48 +30,51 @@ RSpec.describe "Roman numerals" do
     @roman = Roman.new
   end
 
-  it "converts 1 for 'I'" do
-    expect(convert(1)).to eq("I")
+  context "converting base numbers" do
+    it "converts 1 for 'I'" do
+      expect(@roman.convert(1)).to eq("I")
+    end
+    it "converts 5 for 'V'" do
+      expect(@roman.convert(5)).to eq("V")
+    end
+    it "converts 50 for 'L'" do
+      expect(@roman.convert(50)).to eq("L")
+    end
+
+    it "converts 100 for 'C'" do
+      expect(@roman.convert(100)).to eq("C")
+    end
+
+    it "converts 500 for 'D'" do
+      expect(@roman.convert(500)).to eq("D")
+    end
+
+    it "converts 1000 for 'M'" do
+      expect(@roman.convert(1000)).to eq("M")
+    end
   end
 
-  it "converts 2 for 'II'" do
-    expect(convert(2)).to eq("II")
+  context "converting additions" do
+      it "converts 2 for 'II'" do
+      expect(@roman.convert(2)).to eq("II")
+    end
+
+    it "converts 3 for 'III'" do
+      expect(@roman.convert(3)).to eq("III")
+    end
+
+    it "converts 21 for 'XXI'" do
+      expect(@roman.convert(21)).to eq("XXI")
+    end
   end
 
-  it "converts 3 for 'III'" do
-    expect(convert(3)).to eq("III")
-  end
+  conext "converting substraction" do
+      it "converts 4 for 'IV'" do
+      expect(@roman.convert(4)).to eq("IV")
+    end
 
-  it "converts 4 for 'IV'" do
-    expect(convert(4)).to eq("IV")
+    it "converts 9 for 'IX'" do
+      expect(@roman.convert(9)).to eq("IX")
+    end
   end
-
-  it "converts 5 for 'V'" do
-    expect(convert(5)).to eq("V")
-  end
-
-  it "converts 9 for 'IX'" do
-    expect(convert(9)).to eq("IX")
-  end
-
-  it "converts 21 for 'XXI'" do
-    expect(convert(21)).to eq("XXI")
-  end
-
-  it "converts 50 for 'L'" do
-    expect(convert(50)).to eq("L")
-  end
-
-  it "converts 100 for 'C'" do
-    expect(convert(100)).to eq("C")
-  end
-
-  it "converts 500 for 'D'" do
-    expect(convert(500)).to eq("D")
-  end
-
-  it "converts 1000 for 'M'" do
-    expect(convert(1000)).to eq("M")
-  end
-
 end
