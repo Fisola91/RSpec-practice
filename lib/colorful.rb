aedea3a501b7f0bfebd12b53cfb8ac3b6dbd8f3e
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-09 07:52:18 +0200

    colorful method initiated

--- lib/colorful.rb
+++ lib/colorful.rb
@@ -1,12 +1,18 @@
 def colorful?(number)
-  # number should not be above 3 digit
+  # number should not be above 3 digit. Convert to string first
+  return false if number.to_s.length > 3
   # return true if the number is an integer
-  # split the number into an array using chars method and
-  # map over it to convert back to integer
+  return true if [0,1].include? number
+  # split the number into an array using "chars method" and map over it
+  digits_arr = number.to_s.chars.map { |digit| digit.to_i}
+
   # Multiply the consecutive subset of number
+  digits_arr << digits_arr[0] * digits_arr[1]
+  digits_arr << digits_arr[1] * digits_arr[2]
+  digits_arr << digits_arr[0] * digits_arr[2] * digits_arr[3]
+
+  digits_arr.uniq == digits_arr ? true : false
+
   # if product output is uniq return true, hecne false
 
 end
-
-# 263 is a colorful number because (2, 6, 3, 2x6, 6x3, 2x6x3) are all different
-# 236 is not because (2, 3, 6, 2x3, 3x6, 2x3x6) have 6 twice
--- spec/colorful_spec.rb
+++ spec/colorful_spec.rb
@@ -1,5 +1,6 @@
+require "colorful"
 RSpec.describe "make a colorful number" do
   fit "returns true for 263 which is a colorful number" do
-    expect(colorful?()).to eq(true)
+    expect(colorful?(263)).to eq(true)
   end
 end
