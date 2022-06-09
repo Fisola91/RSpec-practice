9a5981bbe6eec42d862abe93708fa5293e822eaf
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-09 08:00:23 +0200

    organized the rspec test with context

--- spec/colorful_spec.rb
+++ spec/colorful_spec.rb
@@ -1,6 +1,7 @@
 require "colorful"
 RSpec.fdescribe "make a colorful number" do
-  it "returns true for 263 which is a colorful number" do
+  context "returning true" do
+    it "returns true for 263 which is a colorful number" do
     expect(colorful?(263)).to eq(true)
   end
 
@@ -11,4 +12,7 @@ RSpec.fdescribe "make a colorful number" do
   it "returns true for 243 which is a colorful number" do
     expect(colorful?(243)).to eq(true)
   end
+end
+
+
 end
