50531ce2cb4b17e2938f253b9893d144523d5b05
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-10 20:27:59 +0200

    refactored the code

--- lib/colorful.rb
+++ lib/colorful.rb
@@ -1,20 +1,20 @@
 def colorful?(number)
+  put self
   # number should not be above 3 digit. Convert to string first
   return false if number.to_s.length > 3
   # return true if the number is an integer
   return true if [0,1].include? number
-  # split the number into an array using "chars method" and map over it
+  # split the number into an array using "digits method" and map over it
   digits_arr = number.digits.reverse
   product = subset_product(digits_arr)
   product.uniq == product ? true : false
 
-  # if product output is uniq return true, hecne false
-
 end
 
 def subset_product(digits)
+   # Multiply the consecutive subset of number
   product = digits.dup
-  # Multiply the consecutive subset of number
+
   if digits.size > 1
     product << digits[0] * digits[1]
   end
