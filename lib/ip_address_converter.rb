404f64e460307c8bf28591770bc8582d4c22316a
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-13 01:12:05 +0200

    ip_address_converter

--- lib/ip_address_converter.rb
+++ lib/ip_address_converter.rb
@@ -1,7 +1,27 @@
-require 'ipaddr'
-def ip_to_num(ip)
-  # IPAddr provides a set of methods to manipulate an IP address.
-  # Both IPv4 and IPv6 are supported.
-  ip = IPAddr.new ip
-  ip.to_i
+# require 'ipaddr'
+# def ip_to_num(ip)
+#   # IPAddr provides a set of methods to manipulate an IP address.
+#   # Both IPv4 and IPv6 are supported.
+#   ip = IPAddr.new ip
+#   ip.to_i
+# end
+
+# def num_to_ip(*num )
+#   num.pack("N").unpack("CCCC").join(".")
+# end
+# # [631271850].pack('N').unpack('CCCC').join('.')
+# # => "37.160.113.170"
+def ip_to_num(ip_address)
+  # convert to array of base 2 binary numbers as strings
+  binary_components = ip_address.split('.') #rjust means right jusrtify
+                      .map { |num| num.to_i.to_s(2).rjust(8, '0') }
+  # join them together and convert back to a base 10 integer
+  binary_components.join.to_i(2)
 end
+p ip_to_num("37.160.113.170")
+def num_to_ip(num)
+  digital_number = num.to_s(2).rjust(32, "0").scan(/.{8}/).map { |ip| ip.to_i(2) }
+  digital_number.join(".")
+
+end
+p num_to_ip(631271850)
--- spec/ip_address_converter_spec.rb
+++ spec/ip_address_converter_spec.rb
@@ -1,10 +1,19 @@
 require "ip_address_converter"
 
 RSpec.describe "ip address converter" do
-  context "convert ip adress to number" do
+  context "convert ip address to number" do
     it "returns number for ip address string" do
       expect(ip_to_num("37.160.113.170")).to eq(631271850)
       expect(ip_to_num("192.168.0.1")).to eq(3232235521)
+      expect(ip_to_num("173.194.40.200")).to eq(2915182792)
+    end
+  end
+
+  context "convert number to ip address" do
+    it "returns string ip address for number" do
+      expect(num_to_ip(631271850)).to eq("37.160.113.170")
+      expect(num_to_ip(3232235521)).to eq("192.168.0.1")
+      expect(num_to_ip(2915182792)).to eq("173.194.40.200")
     end
   end
 end
