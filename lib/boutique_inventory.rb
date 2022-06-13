c65d23309250f1614638983dea49218c96e3a638
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-13 14:52:05 +0200

    initialize boutique_inventory class

---
+++ lib/boutique_inventory.rb
@@ -0,0 +1,3 @@
+class BoutiqueInventory
+
+end
--- spec/shuffle_spec.rb
+++ spec/shuffle_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe "shuffle method" do
   it "should sort an array randomly" do
     input = (1..10).to_a
     expect(shuffle(input)).to contain_exactly(1,2,3,4,5,6,7,8,10,9)
+    expect(shuffle(input.length)).to eq(10)
   end
 end
