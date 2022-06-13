afeba750148e32bbe3b088415ddbcba8b39a5fba
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-13 15:28:22 +0200

    names of boutique inventory listed

--- lib/boutique_inventory.rb
+++ lib/boutique_inventory.rb
@@ -1,3 +1,12 @@
 class BoutiqueInventory
+  def initialize (boutique_inventory = [])
+    @items = boutique_inventory
+  end
 
+  def item_names
+    @items.map do |item|
+      item[:name]
+    end.sort
+
+  end
 end
--- spec/boutique_inventory_spec.rb
+++ spec/boutique_inventory_spec.rb
@@ -1,11 +1,18 @@
 require "boutique_inventory"
 
 RSpec.describe BoutiqueInventory do
-  let(:boutique) {BoutiqueInventory.new([
+  let(:boutique_inventory) {BoutiqueInventory.new([
     {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
     {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
     {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
     {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
   ]) }
-  *
+
+  it "returns a list of the names of the items" do
+      expect(boutique_inventory.item_names).to eq([
+        "Bamboo Socks Cats",
+        "Black Short Skirt",
+        "Maxi Brown Dress",
+        "Red Short Skirt"])
+  end
 end
