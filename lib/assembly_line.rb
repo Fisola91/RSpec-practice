abc49abfedce6ebe24399032c824764f4b6f4835
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-20 00:18:11 +0200

    returns number of working items produced per minute

--- lib/assembly_line.rb
+++ lib/assembly_line.rb
@@ -1,9 +1,14 @@
 class AssemblyLine
+  CARS_PRODUCED_PER_HOUR = 221
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    (@speed * 221) * 90/100.0
+    (@speed * CARS_PRODUCED_PER_HOUR) * 90/100.0
+  end
+
+  def working_items_per_minute
+    (production_rate_per_hour/60).to_i
   end
 end
--- spec/assembly_line_spec.rb
+++ spec/assembly_line_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe AssemblyLine do
   it "returns production rate per hour" do
     expect(assembly_line.production_rate_per_hour).to eq(1193.4)
   end
+
+  it "returns number of working items produced per minute" do
+    expect(assembly_line.working_items_per_minute).to eq(19)
+  end
 end
