6aaa6e1c00107c2dac54f5f14c16a69c2ec50122
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-23 21:16:40 +0200

    moviegoer age 18 or above are allowed to weatch scary movie

--- lib/moviegoer.rb
+++ lib/moviegoer.rb
@@ -6,4 +6,8 @@ class Moviegoer
   def ticket_price
     @age <= 60 ? 15 : 10
   end
+
+  def watch_scary_movie?
+    @age >= 18 ? true : false
+  end
 end
--- spec/moviegoer_spec.rb
+++ spec/moviegoer_spec.rb
@@ -3,8 +3,10 @@ require "moviegoer"
 RSpec.describe Moviegoer do
   let(:moviegoer) { Moviegoer.new(16)}
   let(:moviegoer_2) { Moviegoer.new(65)}
+  let(:moviegoer_3) { Moviegoer.new(21)}
+  let(:moviegoer_4) { Moviegoer.new(17)}
 
-  context "pension" do
+  context "ticket_price" do
     it "returns 10.00 ticket price for old-age pensioneers" do
       expect(moviegoer.ticket_price).to eq(15)
     end
@@ -13,4 +15,12 @@ RSpec.describe Moviegoer do
       expect(moviegoer_2.ticket_price).to eq(10)
     end
   end
+
+  context "scary movie" do
+    it "allows to watch scary movie if 18 and above" do
+      expect(moviegoer_3.watch_scary_movie?).to eq(true)
+    end
+  end
+
+
 end
