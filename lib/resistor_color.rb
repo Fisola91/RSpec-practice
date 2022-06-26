5e6f6af1a452ea6903841b130fad23da430c66c1
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-26 03:00:38 +0200

    validated the resistance_value

--- lib/resistor_color.rb
+++ lib/resistor_color.rb
@@ -12,7 +12,37 @@ class ResistorColors
     white: 9
   }
 
+
   def resistance_value(bands)
     bands.map { |band| BANDS[band.to_sym]}.join.to_i
+
+
+  end
+
+  private
+  def validate_band_name
+    bands.each do |band|
+      unless BANDS.key?(band.to_sym)
+        raise ArgumentError, "wrong band name(check the argument provided)"
+      end
+    end
   end
 end
+
+resis = ResistorColors.new
+p resis.resistance_value(["brown","green"] )
+# BANDS = {
+#   black: 0,
+#   brown: 1,
+#   red: 2,
+#   orange: 3,
+#   yellow: 4,
+#   green: 5,
+#   blue: 6,
+#   violet: 7,
+#   grey: 8,
+#   white: 9
+# }
+
+
+# p BANDS.has_key?(["blue", "yellow"].to_s)
--- spec/resistor_color_spec.rb
+++ spec/resistor_color_spec.rb
@@ -2,8 +2,17 @@ require "resistor_color"
 
 RSpec.describe ResistorColors do
   it "returns two digit number when entered color names as input" do
-    expect(subject.resistance_value(["brown","green"])).to eq(15)
+    expect(subject.resistance_value( ["brown","green"] )).to eq(15)
+    expect(subject.resistance_value( ["red", "orange"])).to eq(23)
+    expect(subject.resistance_value( ["grey", "white"])).to eq(89)
+    expect(subject.resistance_value( ["orange", "yellow"])).to eq(34)
   end
+
+  # it "can check for error" do
+  #   expect(subject.resistance_value( ["blu", "white"])).to eq(69)
+  # end
+
+
 
 end
 # program takes color names as input
