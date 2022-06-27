590a13c9302887e9087b58522e01062ce032ae6d
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-27 07:49:15 +0200

    coffee drink list implemented

--- lib/predicates.rb
+++ lib/predicates.rb
@@ -1,5 +1,10 @@
 class PredicateEnumerables
 
   def coffee_drink?(drink_list)
+    drink_list.include?("coffee") || drink_list.include?("espresso")
+  end
+
+  def correct_guess?(guess_list, number)
+    guess_list.any?(number)
   end
 end
--- spec/predicates_spec.rb
+++ spec/predicates_spec.rb
@@ -1,7 +1,29 @@
 require "predicates"
 RSpec.describe PredicateEnumerables do
-  it "returns true when drink list contain the string coffee" do
+  describe "cofee drink exercise" do
     drink_list = ["coffee", "espressor", "calsberg"]
-    expect(subject.coffee_drink?(drink_list)).to be true
+    it "returns true when coffee is included" do
+      drink_list = ["coffee", "espressor", "calsberg"]
+      expect(subject.coffee_drink?(drink_list)).to be true
+    end
+    it "returns true when espresso is included" do
+      drink_list = ["tea", "espresso", "calsberg"]
+      expect(subject.coffee_drink?(drink_list)).to be true
+    end
+
+    it "returns false when espresso and coffee is not included" do
+      drink_list = ["tea", "wine", "coke"]
+      expect(subject.coffee_drink?(drink_list)).to be false
+    end
+
+    it "returns false when drink_list is empty" do
+      drink_list = []
+      expect(subject.coffee_drink?(drink_list)).to be false
+    end
+    # it "returns true when guess_list equals the answer" do
+    #   guess_list = [ 2, 4, 10, 45, 2]
+    #   number = 10
+    #   expect(subject.correct_guess?(guess_list, number)).to be true
+    # end
   end
 end
