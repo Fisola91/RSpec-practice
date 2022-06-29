f44fef4dd6df6c7f1ba6a6480f3411e554d97f83
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-06-29 10:13:30 +0200

    add_seat_to_row implemented

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -2,4 +2,15 @@ class NestedArray
   def blank_seating_chart(number_of_rows, seats_per_row)
     arr = Array.new(number_of_rows) { Array.new(seats_per_row)}
   end
+
+  def add_seat_to_row(chart, row_index, seat_to_add)
+    chart[row_index].push(seat_to_add)
+    chart
+  end
 end
+# [
+#   #   [nil, nil, nil],
+#   #   [nil, nil, nil]
+#   # ]
+# nest = NestedArray.new
+# nest.def subject.add_seat_to_row(chart, row_index, seat_to_add)
--- spec/nested_array_spec.rb
+++ spec/nested_array_spec.rb
@@ -1,8 +1,13 @@
 require "nested_array"
 RSpec.describe NestedArray do
-  it "returns a 2 dimesional array with nil values" do
-    expect(subject.blank_seating_chart(2, 3)).to eq([[nil, nil, nil],
-                                                                              [nil, nil, nil],])
+  let(:array) { [[nil, nil, nil], [nil, nil, nil]]}
+  it "returns a 2 dimesional array with nil values all through the array" do
+    output = [[nil, nil, nil], [nil, nil, nil]]
+    expect(subject.blank_seating_chart(2, 3)).to eq(output)
+  end
+  it "returns 2 dimensional index where the end of the second row has a value added to it" do
+    output = [[nil, nil, nil], [nil, nil, nil, 1000]]
+    expect(subject.add_seat_to_row(array, 1, 1000)).to eq(output)
   end
 
 end
