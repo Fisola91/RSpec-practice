0205c32a5eb2e5671df7492a0f7d9ed02bd46b3c
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-07-03 23:34:05 +0200

    substring method modified

--- lib/substrings.rb
+++ lib/substrings.rb
@@ -1,4 +1,5 @@
 def substrings(word, array)
+  word.downcase!
   word_array = word.split
   match_array = []
   count_array = []
@@ -9,10 +10,12 @@ def substrings(word, array)
     end
     count = word_array.count { |letter| letter.include?(arr)}
     count_array.push(count)
+    p count_array
     count_array.delete(0)
+    p count_array
   end
   result = match_array.zip(count_array).to_h
-  p result
+
 end
 
 # dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe "#substrings" do
   it "returns a hash listing each substring found in the original string" do
     dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
     expect(substrings("below", dictionary)).to eq( { "below" => 1, "low" => 1 } )
+    expect(substrings("Howdy partner, sit down! How's it going?", dictionary)).to eq( { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 } )
   end
 end
