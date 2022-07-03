d1677d1c233344544c978bd01782bb020a85eabf
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-07-03 23:20:36 +0200

    implemented substring

--- lib/substrings.rb
+++ lib/substrings.rb
@@ -5,17 +5,14 @@ def substrings(word, array)
   # 1. Iterate over the array
   array.each do |arr|
     if word_array.any? { |letter| letter.include?(arr) }
-      p match_array.push(arr)
+      match_array.push(arr)
     end
     count = word_array.count { |letter| letter.include?(arr)}
     count_array.push(count)
     count_array.delete(0)
-    result = match_array.zip(count_array).to_h
-      # word
-
-      # if word.any?(array)
-       # "if word contain substring that is present in dictionary"
   end
+  result = match_array.zip(count_array).to_h
+  p result
 end
 
 # dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
